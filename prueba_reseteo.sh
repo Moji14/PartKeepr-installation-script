e2bcda99d9d57e18c9bf31fd7b04c1d5109f1a3f
User <moji14@hotmail.com>
2021-01-24 12:12:25 +0100

    prueba_reseteo.sh

--- prueba_reseteo.sh
+++ prueba_reseteo.sh
@@ -2,36 +2,34 @@
 
 # filename: prueba_reseteo.sh
 
-# check if the reboot flag file exists.
-
-# We created this file before rebooting.
+# check if the reboot flag file exists (we created this file before rebooting).
 if [[ ! -f /var/run/resume-after-reboot ]]; then
-  	echo "running script for the first time.."
-  	# run your scripts here
 
+# Run your scripts here
+  	echo "Running script for the first time.."
   	# Preparation for reboot
-  	script="bash /prueba_reseteo.sh"
-
-  	# add this script to zsh so it gets triggered immediately after reboot
-  	# change it to .bashrc if using bash shell
-  	echo "$script" >> ~/.bashrc
+	# Add this script to bash so it gets triggered immediately after reboot
+  	phrase="bash ~/PartKeepr-installation-script/prueba_reseteo.sh"
+  	echo "$phrase" >> ~/.bashrc
 
   	# create a flag file to check if we are resuming from reboot.
   	sudo touch /var/run/resume-after-reboot
-
-  	echo "rebooting.."
-  	# reboot here
-  	sleep 10
-	reboot
+	ls /var/run/
+	tail -n 1 ~/.bashrc
+	# Reboot here
+  	echo "Rebooting.."
+  	sleep 3
+	#reboot
 else
-  	echo "resuming script after reboot.."
-	sleep 10
+  	echo "Resuming script after reboot.."
 
-  	# Remove the line that we added in zshrc
-  	sed -i "$script" ~/.bashrc
+  	# Remove the line that we added in bashrc
+	sudo grep -v "bash ~/PartKeepr-installation-script/prueba_reseteo.sh" ~/.bashrc > ~/aux.txt
+	sudo mv ~/aux.txt ~/.bashrc
 
   	# remove the temporary file that we created to check for reboot
   	sudo rm -f /var/run/resume-after-reboot
-
+	ls /var/run/
+	tail -n 1 ~/.bashrc
   	# continue with rest of the script
 fi
